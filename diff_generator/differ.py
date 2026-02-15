6beca3bc7745bc38cd2cf9fa968cbe4f9f08dfa7
shetty-nithin <shettynithin744@gmail.com>
2026-02-16 01:04:45 +0530

    LCS with limitation (is mentioned in the observation.txt file)

--- diff_generator/differ.py
+++ diff_generator/differ.py
@@ -6,9 +6,9 @@ def generate_diff(A, B):
     result = []
 
     while i > 0 and j > 0:
-        print(f"i = {i} and A[i-1] = {A[i-1]}")
-        print(f"j = {j} and B[j-1] = {B[j-1]}\n")
-        if A[i-1] == B[i-1]:
+        #print(f"i = {i} and A[i-1] = {A[i-1]}")
+        #print(f"j = {j} and B[j-1] = {B[j-1]}\n")
+        if A[i-1] == B[j-1]:
             result.append(("equal", A[i-1]))
             i -= 1
             j -= 1
@@ -27,5 +27,4 @@ def generate_diff(A, B):
         result.append(("insert", B[j-1]))
         j -= 1
 
-    print(result)
     return result[::-1]
